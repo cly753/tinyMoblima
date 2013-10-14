8401be22b076a0ba06db64644c39d6bf8c4b772b
cly753 <cly753@gmail.com>
2013-10-14 21:29:42 +0800

    update Moviegoer

--- Moviegoer.java
+++ Moviegoer.java
@@ -1,27 +1,53 @@
+import java.util.ArrayList;
+
 class Moviegoer {
     private String name;
     private String mobileNumber;
     private String emailAddress;
     private Integer age;
-    private Ticket history[];
+    private ArrayList<Ticket> history;
 
     public Moviegoer() {
 
     }
 
+    public boolean setName(String name) {
+        this.name = name;
+        return true;
+    }
     public String getName() {
         return name;
+    }
+
+    public boolean setMoblieNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+        return true;
     }
     public String getMobileNumber() {
         return mobileNumber;
     }
+
+    public boolean setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+        return true;
+    }
     public String getEmailAddress() {
         return emailAddress;
+    }
+
+    public boolean setAge(Integer age) {
+        this.age = age;
+        return true;
     }
     public Integer getAge() {
         return age;
     }
-    public Ticket[] getHistory() {
+
+    public boolean setHistory(Ticket newTicket) {
+        return history.add(newTicket);
+    }
+    public ArrayList<Ticket> getHistory() {
         return history;
     }
+
 }
