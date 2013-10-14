9e1b6ef2ec7392b53e2d79591a9cdc391e086f35
cly753 <cly753@gmail.com>
2013-10-15 00:58:08 +0800

    implement Seat class

---
+++ Seat.java
@@ -0,0 +1,42 @@
+class Seat {
+    private Integer row;
+    private Integer no;
+    private boolean assign;
+    //private Integer moviegoerId;
+    
+    public Seat() {
+
+    }
+
+    public boolean setRow(Integer row) {
+        this.row = row;
+        return true;
+    }
+    public Integer getRow() {
+        return row;
+    }
+
+    public boolean setNo(Integer no) {
+        this.no = no;
+        return true;
+    }
+    public Integer getNo() {
+        return no;
+    }
+
+    public boolean setAssign(boolean assign) {
+        this.assign = assign;
+        return true;
+    }
+    public boolean getAssign() {
+        return assign;
+    }
+
+    // public boolean setMoviegoerId(boolean id) {
+    //     this.moviegoerId = id;
+    //     return true;
+    // }
+    // public Integer getMoviegoerId() {
+    //     return moviegoerId;
+    // }
+}
